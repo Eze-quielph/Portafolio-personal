27b6962dac18b375e3ca35bf75206ea15264a7f4
Ezequiel Benitez <ezequiel.fotografiaa@gmail.com>
2023-12-04 20:22:59 -0300

    links

--- src/app/Components/EmailSection/EmailSection.tsx
+++ src/app/Components/EmailSection/EmailSection.tsx
@@ -53,10 +53,10 @@ const EmailSection = () => {
             try my best to get back to you!
           </p>
           <div className="socials flex flex-row gap-2">
-            <Link href="github.com">
+            <Link href="https://github.com/Eze-quielph">
               <Image src={GithubIcon} alt="Github Icon" />
             </Link>
-            <Link href="linkedin.com">
+            <Link href="https://www.linkedin.com/in/ezequiel-benitez2203/">
               <Image src={LinkedinIcon} alt="Linkedin Icon" />
             </Link>
           </div>
